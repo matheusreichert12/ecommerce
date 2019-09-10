2a2dd87d3f35a37dc52e78b721ac3d0bf64c3c1a
Matheus Vinicius Reichert <matheus.reichert@abase.local>
2019-09-10 16:04:42 -0300

    add arquivos

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text("ShopApp"),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
